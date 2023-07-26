92b510b54a567a89c1ce1e53ba6ed2bdc3210680
max-talanov <max.talanov@gmail.com>
2023-07-26 16:05:39 +0200

    completed cut intput

--- src/repeated_stimulation.py
+++ src/repeated_stimulation.py
@@ -49,6 +49,7 @@ import matplotlib.pyplot as plt
 import logging
 
 
+# Functions
 def get_V3_rate(phase, lo, mid, hi) -> float:
     if 5 < phase < 9:
         return hi
@@ -56,6 +57,26 @@ def get_V3_rate(phase, lo, mid, hi) -> float:
         return mid
     else:
         return lo
+
+
+def identity_cut_chunk(step, hi, lo):
+    ## chunk_number
+    chunk_number = [0]
+    if step < 2:
+        chunk_number = [0]
+    elif step < 3:
+        chunk_number = [0, 1]
+    elif step < 4:
+        chunk_number = [1]
+    elif step < 6:
+        chunk_number = [1, 2]
+    elif step < 7:
+        chunk_number = [2]
+    elif step < 8:
+        chunk_number = [2, 3]
+    elif step < 10:
+        chunk_number = [3]
+    return chunk_number, hi
 
 
 FORMAT = '%(asctime)s %(message)s'
@@ -73,9 +94,9 @@ stop = 1000.0  # end of simulation relative to trial start, in ms
 ###############################################################################
 # The simulation is supposed to take 1s (1000 ms) and is repeated 5 times
 
-
 trial_duration = 1000.0  # trial duration, in ms
 phase_duration = 100.0
+simulation_hill_toe_phases = 4
 num_phases = 10
 num_trials = 5  # number of trials to perform
 v3F_num = 200
@@ -83,6 +104,12 @@ v3F_hi = 200.0  # Hz spiking rate
 v3F_mid = 100.0
 v3F_lo = 50.0  # Hz spiking rate
 bs_num = 100
+cut_num = 100
+
+cut_lo = 5.0  # Hz spiking rate
+cut_hi = 50.0  # Hz spiking rate
+
+cut_chunk = int(cut_num/simulation_hill_toe_phases)
 
 ###############################################################################
 # Third, the network is set up.  We reset the kernel and create a
@@ -95,10 +122,12 @@ bs_num = 100
 nest.ResetKernel()
 # pg_params = {"rate": rate, "start": start, "stop": stop}
 # g_params = {"start": start, "stop": stop, "rate_times": [1, 600, 800], "rate_values": [50, 200, 50]}
-g_params = {"rate":  v3F_lo}
+v3F_g_params = {"rate": v3F_lo}
+cut_g_params = {"rate": cut_lo}
 ## inhomogeneous_poisson_generator
 # pg = nest.Create("poisson_generator", bs_num, params=pg_params)
-pg = nest.Create("poisson_generator", bs_num, params=g_params)
+pg = nest.Create("poisson_generator", bs_num, params=v3F_g_params)
+cut_fiber = nest.Create("poisson_generator", cut_num, params=cut_g_params)
 v3F = nest.Create("hh_psc_alpha_clopath", v3F_num)
 
 ###############################################################################
@@ -110,9 +139,10 @@ sr = nest.Create("spike_recorder")
 # The ``Connect`` function connects the nodes so spikes from pg are collected by
 # the ``spike_recorder`` `sr`
 
-nest.Connect(pg, sr)
-nest.Connect(pg, v3F)
-nest.Connect(v3F, sr)
+# nest.Connect(pg, sr)
+# nest.Connect(pg, v3F)
+# nest.Connect(v3F, sr)
+nest.Connect(cut_fiber, sr)
 
 ###############################################################################
 # Before each trial, we set the ``origin`` of the ``poisson_generator`` to the
@@ -127,6 +157,13 @@ for n in range(num_trials):
         rate = get_V3_rate(ph, v3F_lo, v3F_mid, v3F_hi)
         log.warning(rate)
         pg.rate = rate
+
+        cut_fiber[:].rate = cut_lo
+        cut_chunk_number, cut_freq = identity_cut_chunk(ph, cut_hi, cut_lo)
+        for chn in cut_chunk_number:
+            ## log.info(str(chn*cut_chank) + ": " + str((chn+1)*cut_chank-1))
+            cut_fiber[chn * cut_chunk:(chn + 1) * cut_chunk - 1].rate = cut_freq
+
         nest.Simulate(phase_duration)
 
 ###############################################################################
