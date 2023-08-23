d1bb2a8d0a8a4737f85b9565fb0a40685fa0f1fc
max-talanov <max.talanov@gmail.com>
2023-08-23 14:40:30 +0200

    attached multimeter to muscle with 0 out

--- muscle/iclamp_sim.py
+++ muscle/iclamp_sim.py
@@ -48,37 +48,69 @@ multimeter = nest.Create(
     }
 )
 
-nest.Connect(multimeter, neuron)
+m_multimeter = nest.Create(
+    'multimeter',
+    params={
+        "record_from": [
+            "V_m",
+            "Ca_in",
+            "Act_m",
+            "Act_h",
+            "Inact_n",
+            "Act_p",
+            "Act_mc",
+            "Act_hc",
+            "Ca",
+            "CaT",
+            "At",
+            "Fc",
+            "F"
+        ],
+        #"withtime": True,
+        "interval": 0.1
+    }
+)
+
+
+nest.Connect(m_multimeter, muscle)
 nest.Simulate(150.)
 
 pylab.figure()
 pylab.title('Nest iclamp sim')
 
-pylab.subplot(4, 1, 1)
+pylab.subplot(5, 1, 1)
 pylab.ylabel('Membrane Voltage')
-plot_parameter(multimeter, 'V_m', 'V_m')
+plot_parameter(m_multimeter, 'V_m', 'V_m')
 pylab.legend()
 
-pylab.subplot(4, 1, 2)
+pylab.subplot(5, 1, 2)
 pylab.ylabel('Ca inside')
 pylab.yticks(numpy.arange(0.0001, 0.0010, 0.0002))
-plot_parameter(multimeter, 'Ca_in', 'Ca_in')
+plot_parameter(m_multimeter, 'Ca_in', 'Ca_in')
 pylab.legend()
 
-pylab.subplot(4, 1, 3)
+pylab.subplot(5, 1, 3)
 pylab.ylim(0, 1)
 pylab.ylabel('h, m, n particles')
-plot_parameter(multimeter, 'Act_h', 'h', 'r')
-plot_parameter(multimeter, 'Act_m', 'm', 'g')
-plot_parameter(multimeter, 'Inact_n', 'n', 'b')
+plot_parameter(m_multimeter, 'Act_h', 'h', 'r')
+plot_parameter(m_multimeter, 'Act_m', 'm', 'g')
+plot_parameter(m_multimeter, 'Inact_n', 'n', 'b')
 pylab.legend()
 
-pylab.subplot(4, 1, 4)
+pylab.subplot(5, 1, 4)
 pylab.ylim(0, 1)
 pylab.ylabel('p, mc, hc particles')
-plot_parameter(multimeter, 'Act_p', 'p', 'r')
-plot_parameter(multimeter, 'Act_mc', 'mc', 'g')
-plot_parameter(multimeter, 'Act_hc', 'hc', 'b')
+plot_parameter(m_multimeter, 'Act_p', 'p', 'r')
+plot_parameter(m_multimeter, 'Act_mc', 'mc', 'g')
+plot_parameter(m_multimeter, 'Act_hc', 'hc', 'b')
+pylab.legend()
+
+pylab.subplot(5, 1, 5)
+#pylab.ylim(0, 1)
+pylab.ylabel('Ca, CaT, At, Fc, F')
+plot_parameter(m_multimeter, 'Ca', 'Ca', 'r')
+plot_parameter(m_multimeter, 'CaT', 'CaT', 'g')
+plot_parameter(m_multimeter, 'At', 'At', 'b')
 pylab.legend()
 
 pylab.show()
