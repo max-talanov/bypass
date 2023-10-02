f6f8368a8055513829da1669a9c8541cfd0e0938
nikola-jovisic <nikolajovisic4@gmail.com>
2023-10-02 13:33:12 +0200

    show hdf5 voltages

---
+++ CPG_STDP/py/view_hdf5.py
@@ -0,0 +1,22 @@
+import os
+
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+
+def show_all(path):
+    for entry in os.scandir(path):
+        if not entry.name.endswith('.hdf5'):
+            continue
+        _show_one(entry.path, entry.name)
+def _show_one(path, name):
+    with h5py.File(path, 'r') as file:
+        all = []
+        for i in file:
+            all.extend(np.array(file[i]))
+        all = np.array(all)
+        plt.plot(all)
+        plt.title(name)
+        plt.show()
+
+show_all(r'C:\Users\Korisnik\Desktop')
