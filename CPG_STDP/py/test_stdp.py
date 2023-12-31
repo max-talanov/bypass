3c77973e01cd1f1a1dfb394120d8ff989bed236b
Alina Fedorova <alina-mufler@yandex.ru>
2023-12-31 20:54:18 +0300

    update test_stdp

--- CPG_STDP/py/test_stdp.py
+++ CPG_STDP/py/test_stdp.py
@@ -16,9 +16,8 @@ my_path = os.path.abspath('')
 
 themes = 'light_minimal'
 
-if __name__ == "__main__":
 
-    pc = h.ParallelContext()
+def simple_sim():
     # Настраиваем симуляцию
     h.dt = 0.1
     h.tstop = 10000
@@ -50,53 +49,6 @@ if __name__ == "__main__":
     pstsyn = h.NetCon(cells[1], stdpmech, threshold, delay,
                       -1)  # Feed postsynaptic spikes to the STDP mechanism -- must have weight <0
     h.setpointer(singlesyn._ref_weight[0], 'synweight', stdpmech)
-    # # Настройка симуляции
-    # h.dt = 0.1
-    # h.tstop = 10000
-    #
-    # threshold = 1  # Set voltage threshold
-    # delay = 1  # Set connection delay
-    #
-    # # Создание нейронов с механизмом Hodgkin-Huxley
-    # ncells = 2
-    # cells = []
-    # for _ in range(ncells):
-    #     cell = h.Section(name='hhcell')
-    #     cell.insert('hh')
-    #     cells.append(cell)
-    #
-    # # Создание источника спайков
-    # netstim = h.NetStim()
-    # netstim.number = 250  # Количество спайков
-    # netstim.start = 0  # Время начала генерации спайков
-    # netstim.interval = 10  # Интервал между спайками (в мс)
-    #
-    # synapses = []
-    # for cell in cells:
-    #     syn = h.ExpSyn(cell(1))  # Создание синапса
-    #     syn.tau = 0.3  # Время константы синапса
-    #     syn.e = 0  # Реверсивный потенциал
-    #     synapses.append(syn)
-    #
-    # # Соединяем нейроны синаптическими связями
-    # for i in range(1, ncells):
-    #     nc = h.NetCon(cells[i - 1](0.5)._ref_v, synapses[i], sec=cells[i - 1])
-    #     nc.weight[0] = 0.01  # Вес синапса
-    #     nc.delay = 5  # Задержка синапса
-    # # Подключение NetStim к первому нейрону
-    # ncstim = h.NetCon(netstim, synapses[0])
-    # ncstim.weight[0] = 10
-    #
-    #
-    #
-    # dummy = h.Section()
-    # stdpmech = h.STDP(0, dummy)
-    # singlesyn = h.NetCon(cells[0](0.5)._ref_v, synapses[1], threshold, delay, 0.11)
-    # presyn = h.NetCon(cells[0](0.5)._ref_v, stdpmech, threshold, delay, 1, sec=cells[0])  # Feed presynaptic spikes to the STDP mechanism -- must have weight >0
-    # pstsyn = h.NetCon(cells[1](0.5)._ref_v, stdpmech, threshold, delay, -1, sec=cells[1])  # Feed postsynaptic spikes to the STDP mechanism -- must have weight <0
-    # h.setpointer(singlesyn._ref_weight[0], 'synweight', stdpmech)
-
-    stdpmech.hebbRL()
     stdpmech.verbose = 2
     # Create array to store weight changes
     weight_changes = h.Vector()
@@ -107,11 +59,77 @@ if __name__ == "__main__":
     # Run the simulation
     h.run()
 
+    return weight_changes, time
+
+
+def our_sim():
+    # Настройка симуляции
+    h.dt = 0.1
+    h.tstop = 10000
+
+    threshold = 10  # Set voltage threshold
+    delay = 1  # Set connection delay
+
+    # Создание нейронов с механизмом Hodgkin-Huxley
+    ncells = 2
+    cells = []
+    for _ in range(ncells):
+        cell = h.Section(name='hhcell')
+        cell.insert('hh')
+        cells.append(cell)
+
+    # Создание источника спайков
+    netstim = h.NetStim()
+    netstim.number = 25  # Количество спайков
+    netstim.start = 0  # Время начала генерации спайков
+    netstim.interval = 10  # Интервал между спайками (в мс)
+
+    synapses = []
+    for cell in cells:
+        syn = h.ExpSyn(cell(1))  # Создание синапса
+        syn.tau = 0.3  # Время константы синапса
+        syn.e = 0  # Реверсивный потенциал
+        synapses.append(syn)
+
+    # Соединяем нейроны синаптическими связями
+    for i in range(1, ncells):
+        nc = h.NetCon(cells[i - 1](0.5)._ref_v, synapses[i], sec=cells[i - 1])
+        nc.weight[0] = 1  # Вес синапса
+        nc.delay = 1  # Задержка синапса
+    # Подключение NetStim к первому нейрону
+    ncstim = h.NetCon(netstim, synapses[0])
+    ncstim.weight[0] = 10
+
+
+
+    dummy = h.Section()
+    stdpmech = h.STDP(0, dummy)
+    singlesyn = h.NetCon(cells[0](0.5)._ref_v, synapses[1], threshold, delay, 0.11)
+    presyn = h.NetCon(cells[0](0.5)._ref_v, stdpmech, threshold, delay, 1, sec=cells[0])  # Feed presynaptic spikes to the STDP mechanism -- must have weight >0
+    pstsyn = h.NetCon(cells[1](0.5)._ref_v, stdpmech, threshold, delay, -1, sec=cells[1])  # Feed postsynaptic spikes to the STDP mechanism -- must have weight <0
+    h.setpointer(singlesyn._ref_weight[0], 'synweight', stdpmech)
+    stdpmech.verbose = 2
+    # Create array to store weight changes
+    weight_changes = h.Vector()
+    weight_changes.record(stdpmech._ref_synweight)
+    time = h.Vector()
+    time.record(h._ref_t)
+
+    # Run the simulation
+    h.run()
+    return weight_changes, time
+
+if __name__ == "__main__":
+
+    pc = h.ParallelContext()
+
+    weight_changes, time = our_sim()
+
     # Convert weight_changes to a NumPy array
     weight_changes_array = np.array(weight_changes.as_numpy())
     time_array = np.array(time.as_numpy())
 
-    figur = figure(x_axis_label='time (ms)', y_axis_label='V (mV)')
+    figur = figure(x_axis_label='time (ms)', y_axis_label='weight')
     output_file('change_res.html')
     figur.line(time_array, weight_changes_array, line_width=2)
 
