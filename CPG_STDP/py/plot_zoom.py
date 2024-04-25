248ee31028fc6d0e72b102943abd382213eabde9
Alina-mufler <alina-mufler@yandex.ru>
2024-04-25 14:41:51 +0300

    update file

--- CPG_STDP/py/plot_zoom.py
+++ CPG_STDP/py/plot_zoom.py
@@ -11,8 +11,8 @@ bokeh.sampledata.download()
 
 from bokeh.plotting import figure, output_file, show
 
-sys.path.append('res_weight/')
-my_path = os.path.abspath('res_weight/')
+sys.path.append('last_r/')
+my_path = os.path.abspath('last_r/')
 
 themes = 'light_minimal'
 
@@ -21,7 +21,7 @@ def read():
     volt_data = []
     data_time = []
 
-    for file in fnmatch.filter(os.listdir('./res_weight/'), '*.hdf5'):
+    for file in fnmatch.filter(os.listdir(my_path), '*.hdf5'):
         with h5py.File(my_path+f'\\{file}') as f:
             name = f.filename.split('\\')[-1]
             if name == 'time.hdf5':
@@ -35,10 +35,10 @@ def read():
 
 
     #
-    # with open('time.txt', 'r', encoding='utf-8') as fh:
-    #     for line in fh:
-    #         line = line.rstrip('\n\r')
-    #         data_time.append(float(line))
+    with open(my_path + '/time.txt', 'r', encoding='utf-8') as fh:
+        for line in fh:
+            line = line.rstrip('\n\r')
+            data_time.append(float(line))
     return volt_data, data_time
 
 
@@ -47,8 +47,8 @@ def draw(volt_data, data_time):
 
     for v in volt_data:
         figur = figure(x_axis_label='time (ms)', y_axis_label='weight')
-        output_file(v[0] + '.html')
-        figur.line(data_time[0], v[1], line_width=2)
+        output_file(my_path + '/results/' + v[0] + '.html')
+        figur.line(data_time[:len(v[1])], v[1], line_width=2)
         show(figur)
 
 
