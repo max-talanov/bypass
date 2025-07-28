314e7ed91625658076055b72d652dc350d448c31
Alina Fedorova <57733097+Alina-mufler@users.noreply.github.com>
2025-07-28 19:39:18 +0300

    Update run.sh

--- CPG_STDP/py/run.sh
+++ CPG_STDP/py/run.sh
@@ -3,10 +3,10 @@
 #SBATCH --output=Neuron_calculation.slurmout
 #SBATCH --error=Neuron_calculation.slurmerr
 #SBATCH --nodes=1
-#SBATCH --ntasks=12
+#SBATCH --ntasks=2
 #SBATCH --priority=1000
 #SBATCH --nice=100
 
 #echo "activate env"
-srun --mpi=pmix ../neuron.sif nrniv -mpi -python check_neuron.py
+srun -python check_neuron.py
 
